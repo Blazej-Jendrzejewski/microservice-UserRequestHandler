8d8b63b8433adb428431ccc3c9ac2e4c852b382b
Blazej-Jendrzejewski <blazej.jendrzejewski@gmail.com>
2023-08-28 17:39:26 +0200

    Initial commit

---
+++ src/main/java/com/example/microserviceuserrequesthandler/MicroserviceUserRequestHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceuserrequesthandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUserRequestHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUserRequestHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microserviceuserrequesthandler/MicroserviceUserRequestHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceuserrequesthandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserRequestHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
